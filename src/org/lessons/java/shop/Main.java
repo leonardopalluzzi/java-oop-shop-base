20bc04a0c44f4d63c8eead40f996851974f071ad
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-06-07 17:45:51 +0200

    add corrections

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,15 +1,18 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-        Product prodotto1 = new Product(("iPhone"), "telefono bello ma costoso", 900, 22);
+        Product prodotto1 = new Product(("iPhone"), "telefono bello ma costoso", new BigDecimal(900),
+                new BigDecimal(22));
 
         String printProduct = "Codice prodotto: " + prodotto1.code + " | " + "nome: " + prodotto1.name + " | "
                 + "descrizione: " + prodotto1.description + " | "
-                + "Prezzo (esclusa iva): " + String.format("%.2f", prodotto1.basePrice()) + " euro | "
+                + "Prezzo (esclusa iva): " + String.format("%.2f", prodotto1.getBasePrice()) + " euro | "
                 + "Prezzo (inclusa iva): "
-                + String.format("%.2f", prodotto1.taxedPrice()) + " euro | "
+                + String.format("%.2f", prodotto1.getTaxedPrice()) + " euro | "
                 + "Nome esteso: " + prodotto1.getFullName();
 
         System.out.println(printProduct);
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Product {
@@ -7,29 +9,40 @@ public class Product {
     public int code;
     public String name;
     public String description;
-    public float price;
-    public int iva;
+    public BigDecimal price;
+    public BigDecimal iva;
 
-    public Product(String name, String description, float price, int iva) {
+    public Product(String name, String description, BigDecimal price, BigDecimal iva) {
         Random r = new Random();
         this.code = r.nextInt(5000);
         this.name = name;
         this.description = description;
         this.price = price;
         this.iva = iva;
-    }
+    };
 
-    float basePrice() {
+    public BigDecimal getBasePrice() {
         // stampa prezzo senza iva
         return price;
-    }
+    };
 
-    float taxedPrice() {
+    public BigDecimal getTaxedPrice() {
         // stampa prezzo con iva
-        return price + (price * iva) / 100;
-    }
+        if (price != null && iva != null) {
+            BigDecimal divisor;
+            if (iva.compareTo(BigDecimal.ZERO) > 0) {
+                divisor = new BigDecimal(100);
+            } else {
+                divisor = new BigDecimal(1);
+            }
 
-    String getFullName() {
+            return price.add(price.multiply(iva).divide(divisor)).setScale(2, RoundingMode.DOWN);
+        } else {
+            return null;
+        }
+    };
+
+    public String getFullName() {
         return code + "-" + name;
-    }
+    };
 }
