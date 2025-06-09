083519322bd1f07a604061b7124a86d9781e75e4
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-06-09 22:36:39 +0200

    L05 first snack

---
+++ src/org/lessons/java/shop/Student.java
@@ -0,0 +1,19 @@
+package org.lessons.java.shop;
+
+public class Student {
+
+    private String firstName;
+    private String lasetName;
+    private int age;
+
+    public Student(String firstname, String lastName, int age) {
+        this.firstName = firstname;
+        this.lasetName = lastName;
+        this.age = age;
+    }
+
+    public String getStudet() {
+        return firstName + " " + lasetName + "," + " " + age;
+    }
+
+}
